e79961faa7f79591c42263424536595da3552a12
cryptozeny <cryptozeny@gmail.com>
2019-01-17 05:58:40 +0900

    Sugarchain: fix: (pindexLast == nullptr) at <pow.cpp>
    <chain.cpp>
    void CChain::SetTip(CBlockIndex *pindex) {
        if (pindex == nullptr) {
            vChain.clear();
            return;
        }
        vChain.resize(pindex->nHeight + 1);
        while (pindex && vChain[pindex->nHeight] != pindex) {
            vChain[pindex->nHeight] = pindex;
            pindex = pindex->pprev;
        }
    }

--- src/pow.cpp
+++ src/pow.cpp
@@ -54,7 +54,7 @@ unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHead
     unsigned int nProofOfWorkLimit = UintToArith256(params.powLimit).GetCompact();
 
     // Genesis block
-    if (pindexLast == NULL)
+    if (pindexLast == nullptr) // FIXME.SUGAR // SURE? // <chain.cpp>
         return nProofOfWorkLimit;
 
     {
@@ -82,7 +82,7 @@ unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHead
     }
 
     // Check we have enough blocks
-    if (pindexFirst == NULL)
+    if (pindexFirst == nullptr) // FIXME.SUGAR // SURE? // <chain.cpp>
         return nProofOfWorkLimit;
 
     arith_uint256 bnAvg {bnTot / params.nPowAveragingWindow};
