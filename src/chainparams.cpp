e1ee7fa036bce858c4532ab86c665b297bcbd488
ak <cryptozeny@gmail.com>
2019-02-18 07:43:30 +0900

    Sugarchain: clean up

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -15,7 +15,7 @@
 
 #include <chainparamsseeds.h>
 
-#include <arith_uint256.h>
+#include <arith_uint256.h> // DigiShieldZEC
 
 static CBlock CreateGenesisBlock(const char* pszTimestamp, const CScript& genesisOutputScript, uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward)
 {
