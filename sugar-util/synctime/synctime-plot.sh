fb6f5d3239954763db818eddcb21711754285302
cryptozeny <cryptozeny@gmail.com>
2019-12-09 16:49:01 +0900

    test: OK

--- sugar-util/synctime/synctime-plot.sh
+++ sugar-util/synctime/synctime-plot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## INIT
-FILE_NAME_1="cached_reindex-chainstate.log.csv"
+FILE_NAME_1=$1
 
 ## DRAW PLOT
 OUTPUT_PNG="./png/synctime.png"
@@ -14,9 +14,16 @@ set xlabel "Elapsed Time";
 set ylabel "Block Height";
 
 set datafile separator ","
-     
+
+set xrange [1:*]; set xtics 0, 1;
+
+# 2019-12-06 17:14:47,0:10:00,81558
+# set format x "%H:%M:%S"
+# set timefmt "%Y-%m-%d %H:%M:%S"
+# set format x "%H:%M:%S"
+ 
 plot \
-"$FILE_NAME_1" using 2:3 axis x1y1 w l title "$FILE_NAME_1" lc rgb "#eeeeee" lw 1.0,
+"$FILE_NAME_1" using 0:3 axis x1y1 w l  title "$FILE_NAME_1" lc rgb "#ff0000" lw 1.0,
 # caution at the end: no "\"
 EOFMarker
 
