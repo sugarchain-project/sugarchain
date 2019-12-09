506a36523f6efed93e0a07c512662b03a7073824
cryptozeny <cryptozeny@gmail.com>
2019-12-09 23:09:35 +0900

    failed

--- sugar-util/synctime/synctime-plot.sh
+++ sugar-util/synctime/synctime-plot.sh
@@ -15,15 +15,23 @@ set ylabel "Block Height";
 
 set datafile separator ","
 
-set xrange [1:*]; set xtics 0, 1;
+set yrange [0:*];
+set ytics 0, 100000;
+set format y '%.0f'
 
+# data example
 # 2019-12-06 17:14:47,0:10:00,81558
-# set format x "%H:%M:%S"
-# set timefmt "%Y-%m-%d %H:%M:%S"
-# set format x "%H:%M:%S"
+set xdata time
+set timefmt "%H%M%S"
+set format x "%H:%M:%S"
+# set xrange [0:*]
+# set xrange ["$l_start_dt":"$l_end_dt"]
+
+# set xrange [0:*];
+set xtics 0, 1;
 
 plot \
-"$FILE_NAME_1" using 0:3 axis x1y1 w l  title "$FILE_NAME_1" lc rgb "#ff0000" lw 1.0,
+"$FILE_NAME_1" using 2:3 axis x1y1 w l title "$FILE_NAME_1" lc rgb "#ff0000" lw 1.0,
 # caution at the end: no "\"
 EOFMarker
 
