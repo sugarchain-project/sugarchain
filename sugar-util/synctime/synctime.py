8f1eaaea7aa3c91194ed35ffbc29ff8c8b646455
cryptozeny <cryptozeny@gmail.com>
2019-12-21 01:22:15 +0900

    fix: convert td in seconds

--- sugar-util/synctime/synctime.py
+++ sugar-util/synctime/synctime.py
@@ -25,6 +25,13 @@ with open(logFilePath, "r") as logFile, open(outputFilePath, "w") as csvFile:
             td = dt - startDatetime
             if td >= _td:
                 height = match.group(2)
-                csvFile.write("{0},{1},{2}\n".format(date, td, height))
+                
+                # convert td in seconds: bad conversion, but works...
+                td = str(td)
+                td_in_seconds = sum(x * int(t) for x, t in zip([3600, 60, 1], td.split(":")))
+                
+                # write CSV
+                csvFile.write("{0},{1},{2}\n".format(date, td_in_seconds, height))
+                
                 # output every 10 minutes
                 _td = _td + timedelta(minutes=10)
