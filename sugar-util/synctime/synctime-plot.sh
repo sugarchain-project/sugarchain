74f81c51e6ed2f8d9dd18f437bac554f5a8e9d19
cryptozeny <cryptozeny@gmail.com>
2019-12-21 02:18:02 +0900

    fix: bench

--- sugar-util/synctime/synctime-plot.sh
+++ sugar-util/synctime/synctime-plot.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 ## INIT
-FILE_NAME_1=$1
+FILE_NAME_1="uncached_IBD.log.csv"
+FILE_NAME_2="cached_IBD.log.csv"
 
 ## DRAW PLOT
 OUTPUT_PNG="./png/synctime.png"
@@ -16,14 +17,19 @@ set ylabel "Block Height";
 set datafile separator ","
 
 set xrange [0:*];
-set xtics 0, 1;
+set xtics 0, 3600 rotate by 45 right;
+
+set xdata time
+set timefmt "%s"
+set format x "%H:%M:%S"
 
 set yrange [0:*];
 set ytics 0, 100000;
 set format y '%.0f'
 
 plot \
-"$FILE_NAME_1" using 0:3 axis x1y1 w l  title "$FILE_NAME_1" lc rgb "#ff0000" lw 1.0,
+"$FILE_NAME_1" using 2:3 axis x1y1 w l  title "$FILE_NAME_1" lc rgb "blue" lw 1.0, \
+"$FILE_NAME_2" using 2:3 axis x1y1 w l  title "$FILE_NAME_2" lc rgb "red" lw 1.0,
 # caution at the end: no "\"
 EOFMarker
 
