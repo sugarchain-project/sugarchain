15b6f7be15028d622705255685569bb263d15d5f
kanon <60179867+decryp2kanon@users.noreply.github.com>
2020-02-11 04:47:25 +0900

    cleanup

--- src/net_processing.h
+++ src/net_processing.h
@@ -34,6 +34,7 @@ static constexpr int64_t CHAIN_SYNC_TIMEOUT = 20 * 60; // 20 minutes
 /** How frequently to check for stale tips, in seconds */
 // (10 * 60 / 120) = 5
 static constexpr int64_t STALE_CHECK_INTERVAL = 5; // seconds // FIXME.SUGAR
+
 /** How frequently to check for extra outbound peers and disconnect, in seconds */
 static constexpr int64_t EXTRA_PEER_CHECK_INTERVAL = 45;
 /** Minimum time an outbound-peer-eviction candidate must be connected for, in order to evict, in seconds */
