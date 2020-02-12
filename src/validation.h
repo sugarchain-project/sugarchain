9552daba306b7e8b12bfc2b100035462290b183a
kanon <60179867+decryp2kanon@users.noreply.github.com>
2020-02-12 12:24:23 +0900

    DATABASE_WRITE_INTERVAL & DATABASE_FLUSH_INTERVAL

--- src/validation.h
+++ src/validation.h
@@ -99,9 +99,9 @@ static const int MAX_BLOCKTXN_DEPTH = 10;
  *  want to make this a per-peer adaptive value at some point. */
 static const unsigned int BLOCK_DOWNLOAD_WINDOW = 1024 * 120/2; // FIXME.SUGAR // 120x bitcoin
 /** Time to wait (in seconds) between writing blocks/block index to disk. */
-static const unsigned int DATABASE_WRITE_INTERVAL = 60 * 60;
+static const unsigned int DATABASE_WRITE_INTERVAL = 60 * 60 / 120/2; // FIXME.SUGAR // 120x bitcoin
 /** Time to wait (in seconds) between flushing chainstate to disk. */
-static const unsigned int DATABASE_FLUSH_INTERVAL = 24 * 60 * 60;
+static const unsigned int DATABASE_FLUSH_INTERVAL = 24 * 60 * 60 / 120/2; // FIXME.SUGAR // 120x bitcoin
 /** Maximum length of reject messages. */
 static const unsigned int MAX_REJECT_MESSAGE_LENGTH = 111;
 /** Average delay between local address broadcasts in seconds. */
