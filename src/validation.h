87e3c3a3cb54961829214769a58e36490396f6b1
kanon <60179867+decryp2kanon@users.noreply.github.com>
2020-02-12 13:24:23 +0900

    Revert "MAX_BLOCKS_IN_TRANSIT_PER_PEER & BLOCK_DOWNLOAD_WINDOW"
    This reverts commit 672f8574b188cdbd8e4124f41ac583105335b97b.

--- src/validation.h
+++ src/validation.h
@@ -82,7 +82,7 @@ static const int MAX_SCRIPTCHECK_THREADS = 16;
 /** -par default (number of script-checking threads, 0 = auto) */
 static const int DEFAULT_SCRIPTCHECK_THREADS = 0;
 /** Number of blocks that can be requested at any given time from a single peer. */
-static const int MAX_BLOCKS_IN_TRANSIT_PER_PEER = 16 * 120/2; // FIXME.SUGAR // 120x bitcoin
+static const int MAX_BLOCKS_IN_TRANSIT_PER_PEER = 16;
 /** Timeout in seconds during which a peer must stall block download progress before being disconnected. */
 static const unsigned int BLOCK_STALLING_TIMEOUT = 2;
 /** Number of headers sent in one getheaders result. We rely on the assumption that if a peer sends
@@ -97,7 +97,7 @@ static const int MAX_BLOCKTXN_DEPTH = 10;
  *  Larger windows tolerate larger download speed differences between peer, but increase the potential
  *  degree of disordering of blocks on disk (which make reindexing and pruning harder). We'll probably
  *  want to make this a per-peer adaptive value at some point. */
-static const unsigned int BLOCK_DOWNLOAD_WINDOW = 1024 * 120/2; // FIXME.SUGAR // 120x bitcoin
+static const unsigned int BLOCK_DOWNLOAD_WINDOW = 1024;
 /** Time to wait (in seconds) between writing blocks/block index to disk. */
 static const unsigned int DATABASE_WRITE_INTERVAL = 60 * 60 / 120/2; // FIXME.SUGAR // 120x bitcoin
 /** Time to wait (in seconds) between flushing chainstate to disk. */
