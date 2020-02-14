86834283ef0787874e494756b1544d9b5bfdec47
kanon <60179867+decryp2kanon@users.noreply.github.com>
2020-02-14 15:06:12 +0900

    120x bitcoin // prevent disconnect peers during IBD

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -3581,7 +3581,7 @@ bool PeerLogicValidation::SendMessages(CNode* pto, std::atomic<bool>& interruptM
         // Check for headers sync timeouts
         if (state.fSyncStarted && state.nHeadersSyncTimeout < std::numeric_limits<int64_t>::max()) {
             // Detect whether this is a stalling initial-headers-sync peer
-            if (pindexBestHeader->GetBlockTime() <= GetAdjustedTime() - 24*60*60) {
+            if (pindexBestHeader->GetBlockTime() <= GetAdjustedTime() - (24*60*60)*120) { // FIXME.SUGAR // 120x bitcoin // prevent disconnect peers during IBD
                 if (nNow > state.nHeadersSyncTimeout && nSyncStarted == 1 && (nPreferredDownload - state.fPreferredDownload >= 1)) {
                     // Disconnect a (non-whitelisted) peer if it is our only sync peer,
                     // and we have others we could be using instead.
