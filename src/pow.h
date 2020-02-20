630f6dc3b2e969ae836cf87f880ef69255aafc31
kanon <60179867+decryp2kanon@users.noreply.github.com>
2020-02-21 08:36:53 +0900

    cleanup: remove: pow BTC

--- src/pow.h
+++ src/pow.h
@@ -14,9 +14,6 @@ class CBlockHeader;
 class CBlockIndex;
 class uint256;
 
-unsigned int GetNextWorkRequired_BTC(const CBlockIndex* pindexLast, const CBlockHeader *pblock, const Consensus::Params&);
-unsigned int CalculateNextWorkRequired_BTC(const CBlockIndex* pindexLast, int64_t nFirstBlockTime, const Consensus::Params&);
-
 // DigiShieldZEC
 class arith_uint256;
 unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHeader *pblock, const Consensus::Params&);
