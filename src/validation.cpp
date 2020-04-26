334023ec20acd64955d81ce1685516cd0762c815
kanon <60179867+decryp2kanon@users.noreply.github.com>
2020-04-27 05:24:29 +0900

    printf after check

--- src/validation.cpp
+++ src/validation.cpp
@@ -3045,13 +3045,14 @@ static bool CheckBlockHeader(const CBlockHeader& block, CValidationState& state,
 {
     // Skip pow test until IBD is finished
     if (IsInitialBlockDownload()) {
-        printf("%s SKIP CheckBlockHeader\n", DateTimeStrFormat("%Y-%m-%d %H:%M:%S", GetTime()).c_str());
         return true;
     }
 
     // Check proof of work matches claimed amount
     if (fCheckPOW && !CheckProofOfWork(block.GetPoWHash_cached(), block.nBits, consensusParams))
         return state.DoS(50, false, REJECT_INVALID, "high-hash", false, "proof of work failed");
+
+    printf("%s CheckBlockHeader\n", DateTimeStrFormat("%Y-%m-%d %H:%M:%S", GetTime()).c_str());
 
     return true;
 }
