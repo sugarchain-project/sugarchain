ed08e4b2af210bdc2909ae5074d5260ddc017b03
decryp2kanon <decryp2kanon@gmail.com>
2026-09-17 20:43:04 +0900

    sync: skip Yespower PoW verification during IBD

--- src/validation.cpp
+++ src/validation.cpp
@@ -1122,7 +1122,9 @@ bool ReadBlockFromDisk(CBlock& block, const CDiskBlockPos& pos, const Consensus:
     }
 
     // Check the header
-    if (!CheckProofOfWork(block.GetPoWHash_cached(), block.nBits, consensusParams))
+    // SUGAR: skip expensive Yespower verification during IBD
+    if (!IsInitialBlockDownload() &&
+        !CheckProofOfWork(block.GetPoWHash_cached(), block.nBits, consensusParams))
         return error("ReadBlockFromDisk: Errors in block header at %s", pos.ToString());
 
     return true;
@@ -3045,7 +3047,9 @@ static bool FindUndoPos(CValidationState &state, int nFile, CDiskBlockPos &pos, 
 static bool CheckBlockHeader(const CBlockHeader& block, CValidationState& state, const Consensus::Params& consensusParams, bool fCheckPOW = true)
 {
     // Check proof of work matches claimed amount
-    if (fCheckPOW && !CheckProofOfWork(block.GetPoWHash_cached(), block.nBits, consensusParams))
+    // SUGAR: skip expensive Yespower verification during IBD
+    if (fCheckPOW && !IsInitialBlockDownload() &&
+        !CheckProofOfWork(block.GetPoWHash_cached(), block.nBits, consensusParams))
         return state.DoS(50, false, REJECT_INVALID, "high-hash", false, "proof of work failed");
 
     // FIXME.SUGAR // check PoW: SKIPPED during downloading headers (IBD)
