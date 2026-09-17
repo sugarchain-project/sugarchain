722e0210ae5c529ad03fa359b8bc4cb816b570e6
decryp2kanon <decryp2kanon@gmail.com>
2026-09-17 15:01:21 +0900

    sync: remove minimum chain work check during block download

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -475,7 +475,7 @@ void FindNextBlocksToDownload(NodeId nodeid, unsigned int count, std::vector<con
     // Make sure pindexBestKnownBlock is up to date, we'll need it.
     ProcessBlockAvailability(nodeid);
 
-    if (state->pindexBestKnownBlock == nullptr || state->pindexBestKnownBlock->nChainWork < chainActive.Tip()->nChainWork || state->pindexBestKnownBlock->nChainWork < nMinimumChainWork) {
+    if (state->pindexBestKnownBlock == nullptr || state->pindexBestKnownBlock->nChainWork < chainActive.Tip()->nChainWork) {
         // This peer has nothing interesting.
         return;
     }
