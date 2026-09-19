25a920da0a5368e1d3442b634c923fecb70a0d78
decryp2kanon <decryp2kanon@gmail.com>
2026-09-19 16:51:51 +0900

    Increase block stalling timeout to 20 seconds

--- src/validation.h
+++ src/validation.h
@@ -93,7 +93,7 @@ static const int DEFAULT_SCRIPTCHECK_THREADS = 0;
 static const int MAX_BLOCKS_IN_TRANSIT_PER_PEER = 128; // (was 16)
 
 /** Timeout in seconds during which a peer must stall block download progress before being disconnected. */
-static const unsigned int BLOCK_STALLING_TIMEOUT = 2;
+static const unsigned int BLOCK_STALLING_TIMEOUT = 20;
 /** Number of headers sent in one getheaders result. We rely on the assumption that if a peer sends
  *  less than this number, we reached its tip. Changing this value is a protocol upgrade. */
 static const unsigned int MAX_HEADERS_RESULTS = 2000;
